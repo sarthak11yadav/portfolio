ad825a0c9d0b81353fbec337591b5ecc589e9b14
Saarthak Yadav <52343621+sarthak11yadav@users.noreply.github.com>
2021-07-17 11:22:07 +0530

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls'))
+    path('',include('main.urls')),
+    path('home/',include('base.urls')),
 ]
