88ada7051f90f54df17fe918811034f491645209
Saarthak Yadav <52343621+sarthak11yadav@users.noreply.github.com>
2021-07-19 09:49:05 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio',
-    'django.filters',
+    'mainfile',
 ]
 
 MIDDLEWARE = [
